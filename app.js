6b4dd764d6997f54918049392bf10cf009e7a8be
Jonathan Wu <jonathan@gordiansoftware.com>
2021-03-24 12:27:06 -0700

    add fulfillments

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const url = "https://api.gordiansoftware.com/v2.2";
 
+app.get("/trip/:trip_id", async function (req, res, next) {
+  let response = await fetch(url + "/trip/" + req.params.trip_id, {
+    headers: {
+      Authorization: "Basic " + Base64.encode(process.env.API_KEY + ":"),
+      "Content-Type": "application/json"
+    },
+  })
+  let json = await response.json();
+  res.send(json);
+})
+
 /* New trip */
 app.post("/trip", async function (req, res, next) {
   let response = await fetch(url + "/trip", {
@@ -40,7 +51,7 @@ app.post("/trip", async function (req, res, next) {
         },
         {
           passenger_type: "adult",
-          date_of_birth: "2019-01-01",
+          date_of_birth: "2000-01-01",
           first_names: "Ali",
           surname: "Reza",
           loyalty_program: {
@@ -106,9 +117,11 @@ app.post("/trip", async function (req, res, next) {
   res.send(json);
 });
 
+
+
 /* Fulfill */
 app.post("/fulfill", async function (req, res, next) {
-  var fulfillUrl = url + "/trip/" + req.trip_id + "/fulfill";
+  var fulfillUrl = url + "/trip/" + req.body.trip_id + "/fulfill";
   console.log("fulfill url: " + fulfillUrl);
   let response = await fetch(fulfillUrl, {
     method: "POST",
@@ -116,7 +129,47 @@ app.post("/fulfill", async function (req, res, next) {
       Authorization: "Basic " + Base64.encode(process.env.API_KEY + ":"),
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({})
+    // Using mock data here for this demo.
+    body: JSON.stringify({
+      "contact_details": {
+        "contact_details_type": "passenger",
+        "passenger_id": req.body.passenger_id,
+        "email": "joe@bloggs.com",
+        "phone_number": "1234567890",
+        "address": {
+          "city": "Seattle",
+          "state": "WA",
+          "country": "US",
+          "postal_code": "98105",
+          "street_address_1": "123 Street St.",
+        }
+      },
+      tickets: [
+        {
+          ticket_id: req.body.ticket_id,
+          access_details: {
+            record_locator: '123456'
+          },
+          status: 'booked'
+        }
+      ],
+      "payment_details": {
+        "payment_type": "card",
+        "card_number": 9231836912116018,
+        "cvv": 123,
+        "expiry_month": "05",
+        "expiry_year": "2040",
+        "card_holder_name": "Gordian Software",
+        "network": "visa",
+        "billing_address": {
+          "city": "Seattle",
+          "state": "WA",
+          "country": "US",
+          "postal_code": "98105",
+          "street_address_1": "123 Street St.",
+        }
+      }
+    })
   });
   let json = await response.json();
   res.send(json);
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,22 +7,25 @@ const main = async () => {
   });
 
   const tripJson = await tripResponse.json();
-  const { trip_id, trip_access_token } = tripJson;
+  const { trip_id, trip_access_token, passengers, tickets } = tripJson
 
   if (!trip_id) {
     console.error("failed to create trip: " + JSON.stringify(tripJson));
     return;
   }
   console.log(`trip_id: ${trip_id}, trip_access_token: ${trip_access_token}`);
+  console.log(tripJson)
+  const firstPassenger = passengers[0];
+  const firstTicket = tickets[0];
+  const { passenger_id } = firstPassenger;
+  const { ticket_id } = firstTicket;
 
   const onBasketChange = ({ basket }) => {
     console.log("Step 4: handle basket changes");
     var basketBody = document.getElementById("basket-body");
-    basketBody.innerHTML = "";
     for (var key in basket) {
       const product = basket[key];
       var row = document.createElement("tr");
-      row.classList.remove("invalid");
       var productCell = document.createElement("td");
       var priceCell = document.createElement("td");
       productCell.innerText = product.display_name;
@@ -31,21 +34,6 @@ const main = async () => {
         product.price.total.decimal_places,
         product.price.total.currency
       );
-      var message = document.createElement("message");
-
-      product.validity = product.validity || { state: "valid" }; // TODO: remove
-      if (product.validity.state === "valid") {
-      } else if (product.validity.state === "checking") {
-        row.classList.add("checking");
-        message.innerText = "Checking if this product is still available...";
-      } else if (product.validity.state === "price_changed") {
-        row.classList.add("price_changed");
-        message.innerText = "The price for this product has changed!";
-      } else if (product.validity.state === "unavailable") {
-        row.classList.add("unavailable");
-        message.innerText = "Sorry, this product is no longer available";
-      }
-      priceCell.appendChild(message);
       row.appendChild(productCell);
       row.appendChild(priceCell);
       basketBody.appendChild(row);
@@ -59,6 +47,44 @@ const main = async () => {
     onBasketChange: onBasketChange
   });
 
+  const getTrip = async () => {
+    console.log(("Step 7: Poll the trip"))
+    await fetch(`/trip/${trip_id}`)
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        const fulfillmentBody = document.getElementById("fulfillment-body");
+
+        // Clear the current table showing fulfillment status
+        while (fulfillmentBody.firstChild) {
+          fulfillmentBody.removeChild(fulfillmentBody.firstChild);
+        }
+
+        const { orders = {} } = json;
+        console.log(orders)
+        for (const key in orders) {
+          const order = orders[key];
+          const { display_name, price, status } = order;
+          const row = document.createElement("tr");
+          const productCell = document.createElement("td")
+          const priceCell = document.createElement("td");
+          const statusCell = document.createElement("td");
+          productCell.innerText = display_name
+          priceCell.innerText = window.Gordian.formatPrice(
+            price.total.amount,
+            price.total.decimal_places,
+            price.total.currency
+          );
+          statusCell.innerText = status;
+          row.appendChild(productCell);
+          row.appendChild(priceCell);
+          row.appendChild(statusCell);
+          fulfillmentBody.appendChild(row)
+        }
+      })
+  }
+
   const showUpsell = async () => {
     console.log("Step 3: show the upsell options to the user");
     await window.Gordian.showUpsell({
@@ -71,10 +97,13 @@ const main = async () => {
   };
 
   const fulfill = async () => {
+    // Fulfillment should be done on the server side, not the client side. 
     await fetch("/fulfill", {
       method: "POST",
       body: JSON.stringify({
-        trip_id: trip_id
+        trip_id,
+        passenger_id,
+        ticket_id
       }),
       headers: {
         "Content-Type": "application/json"
@@ -93,6 +122,7 @@ const main = async () => {
   };
 
   document.getElementById("fulfill").addEventListener("click", fulfill);
+  document.getElementById("get-trip").addEventListener("click", getTrip);
 
   await showUpsell();
 };
